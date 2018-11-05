88608e237504c0e090a1a83fe77d6f08c31f2306
Vanessa Prema <vanessabprema@gmail.com>
2018-11-05 21:35:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   if (animal === 'dog') {
-    animal == 'cat'
+    animal = 'cat'
   return animal
 }
 }
