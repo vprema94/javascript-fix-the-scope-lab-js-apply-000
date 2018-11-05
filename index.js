a87cb5af64c5ecd49bd1b20082f739f8af2b6041
Vanessa Prema <vanessabprema@gmail.com>
2018-11-05 21:36:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
+  // Feel free to move things around!
 }
 
 var funkyFunction = function() {
