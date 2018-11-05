7c2eca509e44e2afd8657df729fd3f792adf4cfa
Vanessa Prema <vanessabprema@gmail.com>
2018-11-05 21:34:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  if (animal === 'dog') {
+    animal == 'cat'
   return animal
+}
 }
 
 function add2(n) {
